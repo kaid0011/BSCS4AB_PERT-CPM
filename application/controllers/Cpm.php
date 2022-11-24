65a95aeb08943b4a9fd48c2080276320add834cb
Allana <romero.allana2628@gmail.com>
2022-11-24 18:20:27 +0800

    multiple prereq backward pass 11/24

--- application/controllers/Cpm.php
+++ application/controllers/Cpm.php
@@ -26,12 +26,9 @@ class Cpm extends CI_Controller
             $data[$i]['id'] = $this->input->post($i);
             $data[$i]['desc'] = $this->input->post('task_desc_' . $i);
             $data[$i]['time'] = $this->input->post('task_time_' . $i);
-            if($this->input->post('task_prereq_' . $i) != '-')
-            {
+            if ($this->input->post('task_prereq_' . $i) != '-') {
                 $data[$i]['prereq'] = explode(",", $this->input->post('task_prereq_' . $i));
-            }
-            else
-            {
+            } else {
                 $data[$i]['prereq'][] = -1;
             }
             $data[$i]['es'] = 0;
@@ -45,28 +42,20 @@ class Cpm extends CI_Controller
     }
 
     public function forward_pass($data)
-    { 
-        foreach ($data as $tasks) 
-        {
+    {
+        foreach ($data as $tasks) {
             $id = $tasks['id'];
-            if(in_array("-1", $tasks['prereq'])) //check if first task
+            if (in_array("-1", $tasks['prereq'])) //check if first task
             {
                 $data[$id]['es'] = 0;
                 $data[$id]['ef'] = $tasks['time'];
-            }
-            else
-            {
-                foreach($tasks['prereq'] as $prereq)
-                {
-                    if($prereq != '-1' and count($tasks['prereq']) == 1)
-                    {
+            } else {
+                foreach ($tasks['prereq'] as $prereq) {
+                    if ($prereq != '-1' and count($tasks['prereq']) == 1) {
                         $data[$id]['es'] = $data[$prereq]['ef'];
                         $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
-                    }
-                    elseif($prereq != '-1')
-                    {
-                        if($data[$prereq]['ef'] > $data[$id]['es'])
-                        {
+                    } elseif ($prereq != '-1') {
+                        if ($data[$prereq]['ef'] > $data[$id]['es']) {
                             $data[$id]['es'] = $data[$prereq]['ef'];
                             $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
                         }
@@ -74,61 +63,58 @@ class Cpm extends CI_Controller
                 }
             }
         }
-        //project finish time
-        $data['finish_time'] = max(array_column($data, 'ef'));
+        $data['finish_time'] = max(array_column($data, 'ef'));  //project finish time
 
-        // $this->backward_pass($data);
-        $this->show_result($data);
+        $this->backward_pass($data);
     }
 
-    // public function backward_pass($data)
-    // {
-    //     // reverse $data array
-    //     $cnt = count($data)-1;
-    //     $rev_data = array();
-    //     for($j = $cnt; $j >= 1; $j--)
-    //     {
-    //         $rev_data[] = $data[ .$j];
-    //     }
+    public function backward_pass($data)
+    {
+        // reverse $data array
+        $cnt = count($data) - 1;
+        $rdata = array();
+        $pre = array();
+        for ($j = $cnt; $j >= 1; $j--) {
+            $rdata[] = $data[$j];
+            $pre[] = $data[$j]['prereq'];
+        }
+        $merged_pre = call_user_func_array('array_merge', $pre);
 
-    //     $index = -1;
-    //     foreach($rev_data as $revtasks)
-    //     {
-    //         $rev_id = $revtasks['id'];
-    //         $index++;
-    //         if($index == 0) // check if last task
-    //         {
-    //             $data[ .$rev_id]['lf'] = $data[  . $rev_id]['ef'];
-    //             $data[ .$rev_id]['ls'] = $data[  . $rev_id]['es'];
-    //             $data[ .$rev_id]['isCritical'] = "yes";
-    //         }
-
-    //         $rprereq = $revtasks['prereq'];
-    //         $rprereq_id = array_search($rprereq, array_column($data, 'desc')) + 1;
-    //         if($rprereq != '-')
-    //         {
-    //             if($data[ .$rprereq_id]['lf'] == 0)
-    //             {
-    //                 $data[ .$rprereq_id]['lf'] = $data[ .$rev_id]['ls'];
-    //                 $data[ .$rprereq_id]['ls'] = $data[ .$rprereq_id]['lf'] - $data[ .$rprereq_id]['time'];
-    //                 $data[ .$rprereq_id]['float'] = $data[ .$rprereq_id]['lf'] - $data[ .$rprereq_id]['ef'];
-    //                 if($data[ .$rprereq_id]['float'] == 0)
-    //                 {
-    //                     $data[ .$rprereq_id]['isCritical'] = "yes";
-    //                 }
-    //             }
-    //         } 
-    //     }
-
-    //     // $this->isCritical($data);
-    //     $this->show_result($data);
-    // }
+        foreach ($rdata as $rtasks) {
+            $rid = $rtasks['id'];
+            if (in_array($rid, $merged_pre)) {
+                $p = array_column($data, 'prereq');
+                $key = '';
+                foreach ($p as $k => $v) {
+                    if (in_array($rid, $v)) {
+                        $key = $k;
+                        if ($data[$rid]['lf'] == 0) {
+                            $data[$rid]['lf'] = $data[$key + 1]['ls'];
+                            $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
+                        }
+                        if ($data[$rid]['lf'] > $data[$key + 1]['ls']) {
+                            $data[$rid]['lf'] = $data[$key + 1]['ls'];
+                            $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
+                        }
+                    }
+                }
+            } else {
+                $data[$rid]['lf'] = $data['finish_time'];
+                $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
+            }
+            //compute float and if critical task
+            $data[$rid]['float'] = $data[$rid]['lf'] - $data[$rid]['ef'];
+            if ($data[$rid]['float'] == 0) {
+                $data[$rid]['isCritical'] = "Yes";
+            }
+        }
+        $this->show_result($data);
+    }
 
     public function show_result($data)
     {
         $data['qty'] = count($data);
-        for($j=1; $j < $data['qty']; $j++)
-        {
+        for ($j = 1; $j < $data['qty']; $j++) {
             $project[] = $data[$j];
         }
         $data['project'] = $project;
