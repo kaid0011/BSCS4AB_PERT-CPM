c8e43065e87a7090e810566a356f9f87b650408c
Allana <romero.allana2628@gmail.com>
2022-12-04 15:28:05 +0800

    haha

--- application/controllers/Sample.php
+++ application/controllers/Sample.php
@@ -1,9 +1,20 @@
 <?php
+use gburtini\Distributions\Beta;
 class Sample extends CI_Controller
 {
     public function __construct()
     {
-        parent::__construct();
-        
+        parent::__construct();       
+    }
+
+    public function index()
+    {       
+        $beta = new Beta(1, 100);
+        $draw = $beta->rand();
+        if($draw > 0.5) {
+        echo "We drew a number bigger than 0.5 from a Beta(1,100).\n";
+        }
+
+        echo "hello";
     }
 }
