be18ea0f0f301b6f0e1376a51196ee7f6c1054e4
Allana <romero.allana2628@gmail.com>
2022-12-09 14:40:39 +0800

    comments

--- application/controllers/Normal.php
+++ application/controllers/Normal.php
@@ -13,27 +13,27 @@ class Normal extends CI_Controller
 
         // ASSIGNING VALUES TO ARRAY
         for ($i = 1; $i <= $proj_len; $i++) {
-            $data[$i]['id'] = $this->input->post($i);
-            $data[$i]['desc'] = $this->input->post('task_desc_' . $i);
-            $data[$i]['opt'] = $this->input->post('task_opt_' . $i);
-            $data[$i]['ml'] = $this->input->post('task_ml_' . $i);
-            $data[$i]['pes'] = $this->input->post('task_pes_' . $i);
-            $data[$i]['time'] = 0;
-            if ($this->input->post('task_prereq_' . $i) != '-') {
-                $data[$i]['prereq'] = explode(",", $this->input->post('task_prereq_' . $i));
-            } else {
-                $data[$i]['prereq'][] = -1;
+            $data[$i]['id'] = $this->input->post($i);   // Task ID
+            $data[$i]['desc'] = $this->input->post('task_desc_' . $i);  // Task Description
+            $data[$i]['opt'] = $this->input->post('task_opt_' . $i);    // Optimistic 
+            $data[$i]['ml'] = $this->input->post('task_ml_' . $i);      // Most Likely
+            $data[$i]['pes'] = $this->input->post('task_pes_' . $i);    // Pessimistic
+            $data[$i]['time'] = 0;  // Duration
+            if ($this->input->post('task_prereq_' . $i) != '-') {   // If not 1st task
+                $data[$i]['prereq'] = explode(",", $this->input->post('task_prereq_' . $i));    // Turn prereqs into array
+            } else {    //If first task
+                $data[$i]['prereq'][] = -1; // Turn prereq into array and replace with -1
             }
             // $data[$i]['sd'] = 0;
-            $data[$i]['es'] = 0;
-            $data[$i]['ef'] = 0;
-            $data[$i]['ls'] = 0;
-            $data[$i]['lf'] = 0;
-            $data[$i]['float'] = 0;
-            $data[$i]['isCritical'] = "No";
-            $data[$i]['N'] = $this->input->post('N');
+            $data[$i]['es'] = 0;    // Earliest Start
+            $data[$i]['ef'] = 0;    // Earliest Finish
+            $data[$i]['ls'] = 0;    // Latest Start
+            $data[$i]['lf'] = 0;    // Latest Finish
+            $data[$i]['float'] = 0; // Float
+            $data[$i]['isCritical'] = "No"; // Critical task or not
+            $data[$i]['N'] = $this->input->post('N');   // Number of trials
         }
-        $this->alphabeta($data);
+        $this->alphabeta($data);    // proceed to alphabeta function to compute task duration
     }
 
     public function alphabeta($data)
@@ -46,77 +46,92 @@ class Normal extends CI_Controller
             $b = $ab['pes'];
             $pd = 'normal';
             $N = $ab['N'];
-            
-            // echo $id."<br>";
 
+            // Pass values to python to compute task duration
             $command = escapeshellcmd("python pd.py $pd $a $m $b $N");
             $output = shell_exec($command);
 
-            // echo $output."<br>";
-            $data[$id]['time'] = round($output, 2);
-            // echo "Time: ".$data[$id]['time']."<br>";
+            $data[$id]['time'] = round($output, 2); // assign python output to task duration
         }
-        $this->forward_pass($data);
+        $this->forward_pass($data); // proceed to forward pass
     }
 
     public function forward_pass($data)
     {
+        /* 
+            ---FORWARD PASS---
+            EF = ES + T
+            * If first task, EF = T
+
+            ES = EF of prerequisite
+            * If more than one prerequisite, get highest 
+        */
         foreach ($data as $tasks) {
             $id = $tasks['id'];
             if (in_array("-1", $tasks['prereq'])) //check if first task
             {
-                $data[$id]['es'] = 0;
-                $data[$id]['ef'] = $tasks['time'];
-            } else {
-                foreach ($tasks['prereq'] as $prereq) {
-                    if ($prereq != '-1' and count($tasks['prereq']) == 1) {
-                        $data[$id]['es'] = $data[$prereq]['ef'];
-                        $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
-                    } elseif ($prereq != '-1') {
-                        if ($data[$prereq]['ef'] > $data[$id]['es']) {
-                            $data[$id]['es'] = $data[$prereq]['ef'];
-                            $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
+                $data[$id]['es'] = 0;   //ES = 0
+                $data[$id]['ef'] = $tasks['time'];      //EF = duration
+            } else {    //if not first task
+                foreach ($tasks['prereq'] as $prereq) {     // Loop through the prereq array of each task 
+                    if ($prereq != '-1' and count($tasks['prereq']) == 1) {     // If only 1 prereq
+                        $data[$id]['es'] = $data[$prereq]['ef'];                // ES = EF of prereq
+                        $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];   // EF = ES + Duration
+                    } elseif ($prereq != '-1') {    // If multiple prereqs
+                        if ($data[$prereq]['ef'] > $data[$id]['es']) {      // if prereq's EF is greater than current task's ES
+                            $data[$id]['es'] = $data[$prereq]['ef'];        // ES = EF of prereq
+                            $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];   // EF = ES + duration
                         }
                     }
                 }
             }
         }
-        $data['finish_time'] = max(array_column($data, 'ef'));  //project finish time
+        $data['finish_time'] = max(array_column($data, 'ef'));  //project finish time = maximum EF
 
-        $this->backward_pass($data);
+        $this->backward_pass($data);    // proceed to backward pass
     }
 
     public function backward_pass($data)
     {
+        /*
+            ---BACKWARD PASS---
+            LF
+            * If not a prerequisite of any task, LF = Project Finish Time
+            * Otherwise, LS of successor
+            ** If multiple successors, get lowest
+
+            LS = LF - T
+        */
+
         // reverse $data array
         $cnt = count($data) - 1;
         $rdata = array();
         $pre = array();
         for ($j = $cnt; $j >= 1; $j--) {
-            $rdata[] = $data[$j];
-            $pre[] = $data[$j]['prereq'];
+            $rdata[] = $data[$j];           // assign reversed $data array to $rdata
+            $pre[] = $data[$j]['prereq'];   // assign all prereqs to $pre associative array
         }
-        $merged_pre = call_user_func_array('array_merge', $pre);
+        $merged_pre = call_user_func_array('array_merge', $pre);    // merge $pre array to easily locate if a task is a prereq of any task
 
         foreach ($rdata as $rtasks) {
             $rid = $rtasks['id'];
-            if (in_array($rid, $merged_pre)) {
+            if (in_array($rid, $merged_pre)) {  // if current task is a prereq of any task
                 $p = array_column($data, 'prereq');
                 $key = '';
                 foreach ($p as $k => $v) {
                     if (in_array($rid, $v)) {
                         $key = $k;
-                        if ($data[$rid]['lf'] == 0) {
+                        if ($data[$rid]['lf'] == 0) {   // if LF not yet computed
                             $data[$rid]['lf'] = $data[$key + 1]['ls'];
                             $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
                         }
-                        if ($data[$rid]['lf'] > $data[$key + 1]['ls']) {
+                        if ($data[$rid]['lf'] > $data[$key + 1]['ls']) {    // if current task's LF is greater than succesor's LS
                             $data[$rid]['lf'] = $data[$key + 1]['ls'];
                             $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
                         }
                     }
                 }
-            } else {
+            } else {    // if not a prereq of any task
                 $data[$rid]['lf'] = $data['finish_time'];
                 $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
             }
@@ -126,10 +141,7 @@ class Normal extends CI_Controller
                 $data[$rid]['isCritical'] = "Yes";
             }
         }
-        // var_dump($data);
-        flush();
-        print_r($data);
-        $this->show_result($data);
+        $this->show_result($data);  // proceed to show_result
     }
 
     public function show_result($data)
--- application/controllers/Pert.php
+++ application/controllers/Pert.php
@@ -39,70 +39,88 @@ class Pert extends CI_Controller
         foreach ($data as $time)
         {
             $tid = $time['id'];
-            $data[$tid]['time'] = ($time['opt'] + (4 * $time['ml']) + $time['pes']) / 6;
-            $data[$tid]['sd'] = ($time['pes'] - $time['opt']) / 6;
+            $data[$tid]['time'] = ($time['opt'] + (4 * $time['ml']) + $time['pes']) / 6;    // compute task mean
+            $data[$tid]['sd'] = ($time['pes'] - $time['opt']) / 6;                          // compute task standard deviation
         }
-        $this->forward_pass($data);
+        $this->forward_pass($data);     // proceed to forward pass
     }
 
     public function forward_pass($data)
     {
+        /* 
+            ---FORWARD PASS---
+            EF = ES + T
+            * If first task, EF = T
+
+            ES = EF of prerequisite
+            * If more than one prerequisite, get highest 
+        */
         foreach ($data as $tasks) {
             $id = $tasks['id'];
             if (in_array("-1", $tasks['prereq'])) //check if first task
             {
-                $data[$id]['es'] = 0;
-                $data[$id]['ef'] = $tasks['time'];
-            } else {
-                foreach ($tasks['prereq'] as $prereq) {
-                    if ($prereq != '-1' and count($tasks['prereq']) == 1) {
-                        $data[$id]['es'] = $data[$prereq]['ef'];
-                        $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
-                    } elseif ($prereq != '-1') {
-                        if ($data[$prereq]['ef'] > $data[$id]['es']) {
-                            $data[$id]['es'] = $data[$prereq]['ef'];
-                            $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];
+                $data[$id]['es'] = 0;   //ES = 0
+                $data[$id]['ef'] = $tasks['time'];      //EF = duration
+            } else {    //if not first task
+                foreach ($tasks['prereq'] as $prereq) {     // Loop through the prereq array of each task 
+                    if ($prereq != '-1' and count($tasks['prereq']) == 1) {     // If only 1 prereq
+                        $data[$id]['es'] = $data[$prereq]['ef'];                // ES = EF of prereq
+                        $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];   // EF = ES + Duration
+                    } elseif ($prereq != '-1') {    // If multiple prereqs
+                        if ($data[$prereq]['ef'] > $data[$id]['es']) {      // if prereq's EF is greater than current task's ES
+                            $data[$id]['es'] = $data[$prereq]['ef'];        // ES = EF of prereq
+                            $data[$id]['ef'] = $data[$id]['es'] + $tasks['time'];   // EF = ES + duration
                         }
                     }
                 }
             }
         }
-        $data['finish_time'] = max(array_column($data, 'ef'));  //project finish time
+        $data['finish_time'] = max(array_column($data, 'ef'));  //project finish time = maximum EF
 
-        $this->backward_pass($data);
+        $this->backward_pass($data);    // proceed to backward pass
     }
 
     public function backward_pass($data)
     {
+        /*
+            ---BACKWARD PASS---
+            LF
+            * If not a prerequisite of any task, LF = Project Finish Time
+            * Otherwise, LS of successor
+            ** If multiple successors, get lowest
+
+            LS = LF - T
+        */
+
         // reverse $data array
         $cnt = count($data) - 1;
         $rdata = array();
         $pre = array();
         for ($j = $cnt; $j >= 1; $j--) {
-            $rdata[] = $data[$j];
-            $pre[] = $data[$j]['prereq'];
+            $rdata[] = $data[$j];           // assign reversed $data array to $rdata
+            $pre[] = $data[$j]['prereq'];   // assign all prereqs to $pre associative array
         }
-        $merged_pre = call_user_func_array('array_merge', $pre);
+        $merged_pre = call_user_func_array('array_merge', $pre);    // merge $pre array to easily locate if a task is a prereq of any task
 
         foreach ($rdata as $rtasks) {
             $rid = $rtasks['id'];
-            if (in_array($rid, $merged_pre)) {
+            if (in_array($rid, $merged_pre)) {  // if current task is a prereq of any task
                 $p = array_column($data, 'prereq');
                 $key = '';
                 foreach ($p as $k => $v) {
                     if (in_array($rid, $v)) {
                         $key = $k;
-                        if ($data[$rid]['lf'] == 0) {
+                        if ($data[$rid]['lf'] == 0) {   // if LF not yet computed
                             $data[$rid]['lf'] = $data[$key + 1]['ls'];
                             $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
                         }
-                        if ($data[$rid]['lf'] > $data[$key + 1]['ls']) {
+                        if ($data[$rid]['lf'] > $data[$key + 1]['ls']) {    // if current task's LF is greater than succesor's LS
                             $data[$rid]['lf'] = $data[$key + 1]['ls'];
                             $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
                         }
                     }
                 }
-            } else {
+            } else {    // if not a prereq of any task
                 $data[$rid]['lf'] = $data['finish_time'];
                 $data[$rid]['ls'] = $data[$rid]['lf'] - $rtasks['time'];
             }
@@ -112,7 +130,7 @@ class Pert extends CI_Controller
                 $data[$rid]['isCritical'] = "Yes";
             }
         }
-        $this->show_result($data);
+        $this->show_result($data);  // proceed to show_result
     }
 
     public function show_result($data)
