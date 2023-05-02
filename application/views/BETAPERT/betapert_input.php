d5496d8100fc46a69dee9c87271b80ad048c957e
Allana <romero.allana2628@gmail.com>
2023-05-02 19:39:33 +0800

    Removed required condition for description

--- application/views/BETAPERT/betapert_input.php
+++ application/views/BETAPERT/betapert_input.php
@@ -34,7 +34,7 @@
                 <tr>
                     <td><input type="text1" name="<?php echo $i; ?>" value="<?php echo $i; ?>" readonly></td>
                     <!-- <td><input type="text" name="task_desc_<?php echo $i; ?>" required></td> -->
-                    <td><textarea  name = "task_desc_<?php echo $i; ?>" required></textarea></td>
+                    <td><textarea  name = "task_desc_<?php echo $i; ?>"></textarea></td>
                     <td><input type="number" name="task_opt_<?php echo $i; ?>" step="any"  min="1" max="20" oninput="validity.valid||(value='');" required></td>
                     <td><input type="number" name="task_ml_<?php echo $i; ?>" step="any" min="1" max="20" oninput="validity.valid||(value='');" required></td>
                     <td><input type="number" name="task_pes_<?php echo $i; ?>" step="any"  min="1" max="20" oninput="validity.valid||(value='');" required></td>
