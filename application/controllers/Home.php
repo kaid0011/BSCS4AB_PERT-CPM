1bd0da676941db56effb0a2a1302352e034a6d32
Kim Bagasona <kimdiannebagasona@gmail.com>
2023-05-12 20:26:27 +0800

    update left home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,8 +8,11 @@ class Home extends CI_Controller
 
     public function index()
     {
+        $arr = array(
+            'pagename' => 'HOME TOH',
+        );
         $this->load->view('template/header');
-        $this->load->view('home/homepage');
+        $this->load->view('home/homepage', $arr);
         $this->load->view('template/footer');
     }
 }
