7627c1559851f12de7b610700b3625eedcc6b009
Kim Bagasona <kimdiannebagasona@gmail.com>
2023-05-14 21:14:45 +0800

    fixed simulations

--- application/views/triangular/triangular_output.php
+++ application/views/triangular/triangular_output.php
@@ -130,7 +130,7 @@
 
         <div class="export">
             <!-- Export Simulation Values Excel File -->
-            <form action="<?php echo base_url('export') ?>" method="post">
+            <form action="<?php echo base_url('export/simu') ?>" method="post">
                 <?php
                 foreach ($project as $sim) {
                     $id = $sim['id'];
