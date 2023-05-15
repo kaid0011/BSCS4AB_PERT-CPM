2793a1ca8c7cb2ce6433b111635fa189a2d782c8
Kim Bagasona <kimdiannebagasona@gmail.com>
2023-05-15 23:00:17 +0800

    manual

---
+++ application/controllers/User_manual.php
@@ -0,0 +1,16 @@
+<?php
+class User_manual extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $temp['title'] = 'WAPS with Simulation';
+        $this->load->view('template/header', $temp);
+        $this->load->view('manual/user_manual');
+        $this->load->view('template/footer');
+    }
+}
---
+++ application/views/manual/user_manual.php
@@ -0,0 +1,17 @@
+<h1>How to Use WAPS with Simulation</h1>
+<h2>Project Evaluation Review Technique (PERT)</h2>
+<div class="howto">
+                    <h2> How To?</h2>
+                    <ul>
+                        <li>
+                            <p>Enter the Number of Activities of your project.</p>
+                        </li>
+                        <li>
+                            <p>Choose your desired Unit of Time: Days, Weeks, or Months.</p>
+                        </li>
+                        <li>
+                            <p>Click 'Generate Table' to generate a table to input your project details.</p>
+                        </li>
+                    </ul>
+                </div>
+                
