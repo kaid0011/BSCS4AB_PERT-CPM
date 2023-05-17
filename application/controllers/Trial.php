6f46d5fafc8487135ea956752c5e0c0011364029
Kim <kimdiannebagasona@gmail.com>
2023-05-18 00:42:01 +0800

    created UI ang graph for PERT chart

--- application/controllers/Trial.php
+++ application/controllers/Trial.php
@@ -9,6 +9,8 @@ class Trial extends CI_Controller
     public function index()
     {
         $temp['title'] = 'WAPS with Simulation';
+        $this->load->view('template/header', $temp);
         $this->load->view('trial');
+        $this->load->view('template/footer');
     }
 }
