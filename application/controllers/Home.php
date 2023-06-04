daee7882c3d73661018f4e3b1b18bcc9e57832d5
Kim <kimdiannebagasona@gmail.com>
2023-06-04 19:07:11 +0800

    trial pwa

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,7 +7,10 @@ class Home extends CI_Controller
     }
 
     public function index()
-    {
+    { ?>
+        <link rel="manifest" href="<?= base_url() ?>/manifest.json" />
+  <script>navigator.serviceWorker.register("<?= base_url() ?>/service-worker.js")</script> 
+  <?php
         $temp['title'] = 'WAPS with Simulation';
         $this->load->view('template/header', $temp);
         $this->load->view('home/homepage');
